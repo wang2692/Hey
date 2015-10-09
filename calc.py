135b591fe9e336e3db5824c19a7b5ecbd46bb2d9
Yicheng <wang2692@purdue.edu>
2015-10-09 13:10:12 -0400

    fggh

--- calc.py
+++ calc.py
@@ -37,7 +37,7 @@ def div(a, b):
 # a -- modulo
 # b -- modulor
 def mod(a, b):
-	return a % b
+    return a % b
 
 # -------------------------------------------------------- #
 
@@ -72,8 +72,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-		elif (op == "%"):
-			print "Modulus: ", mod(a, b)
+        elif (op == "%"):
+            print "Modulus: ", mod(a, b)
         else:
             print "Invalid operation..."
 
